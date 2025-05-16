539cd09eca69fd35c9b287c684037036c10b1989
mohamedhamdy9966 <mh7008002@gmail.com>
2025-05-16 16:00:22 +0300

    fix-banner

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -9,8 +9,7 @@ const MainBanner = () => {
       <img src={assets.main_banner_bg_sm} alt="smallBanner" className="w-full md:hidden" />
       <div className="absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-24 md:pb-0 px-4 md:pl-18 lg:pl-24">
         <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15">
-          Best Cosmo Medical Best Cosmo Medical Best Cosmo Medical Best Cosmo
-          Medical
+          Best Cosmo Medical
         </h2>
         <div className="flex items-center mt-6 font-medium">
           <Link
